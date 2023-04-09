3bed60d1412fff66fbcf2d155acd33b4dd782795
Qing Bin Liang <leunghb@hotmail.com>
2023-04-08 18:00:10 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "YOUR-NAME is the champion!";
+document.body.appendChild(newHeader);
